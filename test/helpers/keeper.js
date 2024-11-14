2083928f43c5f1fc51cdfc19f914e76ef087b644
Sergey Kudashev <kudashevs@gmail.com>
2024-11-14 20:27:50 +0300

    Update keeper remove a redundant descriptor

--- test/helpers/keeper.js
+++ test/helpers/keeper.js
@@ -5,13 +5,12 @@ const FIXTURE_PATH = path.resolve(__dirname, '../temp/test-storage.tmp');
 
 class Fixture {
   static open() {
-    Fixture.fd = fs.openSync(FIXTURE_PATH, 'w');
+    fs.openSync(FIXTURE_PATH, 'w');
   }
 
   static write(data) {
     try {
-      const file = Fixture.fd || FIXTURE_PATH;
-      fs.writeFileSync(file, data, {encoding: 'utf-8', flag: 'w'});
+      fs.writeFileSync(FIXTURE_PATH, data, {encoding: 'utf-8', flag: 'w'});
     } catch (err) {
       throw new Error(`Cannot write to file ${FIXTURE_PATH} because ${err.message}`);
     }
@@ -30,7 +29,6 @@ class Fixture {
   }
 
   static close() {
-    fs.closeSync(Fixture.fd);
     fs.unlinkSync(FIXTURE_PATH);
   }
 }
